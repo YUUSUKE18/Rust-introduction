e437ae61e5a68cf720a18608a428c356fdf8341c
YUUSUKE18 <yuusuke.o.work.1018@gmail.com>
2024-10-05 17:31:55 +0900

    solve 03_if_else

---
+++ 100-exercises/exercises/02_basic_calculator/03_if_else/target/debug/deps/if_else-aa6bd6922456d226.d
@@ -0,0 +1,5 @@
+/Users/yuusuke/Desktop/github/Rust-introduction/100-exercises/exercises/02_basic_calculator/03_if_else/target/debug/deps/if_else-aa6bd6922456d226: src/lib.rs
+
+/Users/yuusuke/Desktop/github/Rust-introduction/100-exercises/exercises/02_basic_calculator/03_if_else/target/debug/deps/if_else-aa6bd6922456d226.d: src/lib.rs
+
+src/lib.rs:
